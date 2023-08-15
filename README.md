84c6971e85543d0eb55a573592aaabb33cab1acf
DESKTOP-1AGALA0\Atom <sereepap2029@gmail.com>
2023-08-16 02:49:59 +0700

    check point

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # No-Black-Box-Machine-Learning
+
+my repo for learn this course https://www.youtube.com/watch?v=vDDjtwQDw2k&ab_channel=freeCodeCamp.org
---
+++ web/js/sketchpad.js
@@ -0,0 +1,42 @@
+class SketchPad {
+  constructor(container, size = 400) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = size;
+    this.canvas.height = size;
+    this.canvas.style = `
+            background-color:white;
+            box-shadow:0px 0px 10px 2px black;
+        `;
+    container.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
+    this.#addEventListener();
+    this.path = [];
+    this.isDrawing = false;
+  }
+  #addEventListener() {
+    this.canvas.onmousedown = (evt) => {
+      const mouse = this.#getMouse(evt);
+      this.path = [mouse];
+      this.isDrawing = true;
+    };
+    this.canvas.onmousemove = (evt) => {
+      if (this.isDrawing) {
+        const mouse = this.#getMouse(evt);
+        this.path.push(mouse);
+        this.#redraw();
+      }
+    };
+    this.canvas.onmouseup = (evt) => {
+      this.isDrawing = false;
+    };
+  }
+  #getMouse = (evt) => {
+    const rect = this.canvas.getBoundingClientRect();
+    let mouse = [Math.round(evt.clientX - rect.left), Math.round(evt.clientY - rect.top)];
+    return mouse;
+  };
+  #redraw = () => {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    draw.path(this.ctx, this.path);
+  };
+}
